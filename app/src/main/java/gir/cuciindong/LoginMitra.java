ddfc6b6f3c9e3d31acdc37f60a399142ab912548
ikhsan solihin <ikhsansolihin49@gmail.com>
2018-05-28 20:49:17 +0700

    yang baru

--- app/src/main/java/gir/cuciindong/LoginMitra.java
+++ app/src/main/java/gir/cuciindong/LoginMitra.java
@@ -1,6 +1,7 @@
 package gir.cuciindong;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
@@ -9,9 +10,13 @@ import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginMitra extends AppCompatActivity implements View.OnClickListener {
+public class LoginMitra extends AppCompatActivity {
 
     private final AppCompatActivity activity = LoginMitra.this;
 
@@ -20,25 +25,58 @@ public class LoginMitra extends AppCompatActivity implements View.OnClickListene
 //    private TextInputLayout textInputLayoutEmail;
 //    private TextInputLayout textInputLayoutPassword;
 
-    private TextInputEditText et_email;
-    private TextInputEditText et_password;
+    private EditText et_email;
+    private EditText et_password;
 
-    private AppCompatButton btn_login;
+    private Button btn_login;
 
-    private AppCompatTextView btn_register_p;
+    private Button btn_register_p;
 
     private InputValidation inputValidation;
-    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_mitra);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
         initViews();
-        initListeners();
         initObjects();
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = "SELECT password FROM akun WHERE email='" + et_email.getText().toString() + "'";
+                Cursor c1 = DatabaseOpenHelper.rawQuery(query);
+                String password_db;
+                if (c1 != null && c1.getCount() != 0) {
+                    if (c1.moveToFirst()) {
+                        do {
+                            password_db = c1.getString(2);
+                            Toast.makeText(LoginMitra.this, "password db : " + password_db, Toast.LENGTH_SHORT).show();
+                        } while (c1.moveToNext());
+                    }
+                }
+                c1.close();
+//                Toast.makeText(LoginMitra.this, "password = "+ password_db, Toast.LENGTH_SHORT).show();
+//                if(et_password.getText().toString() == c1.getString(c1.getColumnIndex("password"))){
+//                    Intent intent = new Intent(LoginMitra.this, HomeMitra.class);
+//                    startActivity(intent);
+//                }
+//                if(et_password.getText().toString() != c1.getString(c1.getColumnIndex("password"))){
+//                    Toast.makeText(LoginMitra.this, "Email atau password salah", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
+
+        btn_register_p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginMitra.this, RegisterMitra1.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     /**
@@ -46,28 +84,23 @@ public class LoginMitra extends AppCompatActivity implements View.OnClickListene
      */
     private void initViews() {
 
-        et_email = (TextInputEditText) findViewById(R.id.et_email_login_mitra);
-        et_password = (TextInputEditText) findViewById(R.id.et_password_login_mitra);
+        et_email = (EditText) findViewById(R.id.et_email_login_mitra);
+        et_password = (EditText) findViewById(R.id.et_password_login_mitra);
 
-        btn_login = (AppCompatButton) findViewById(R.id.btn_login_mitra);
+        btn_login = (Button) findViewById(R.id.btn_login_mitra);
 
-        btn_register_p = (AppCompatTextView) findViewById(R.id.btn_register_p);
+        btn_register_p = (Button) findViewById(R.id.btn_register);
 
     }
 
     /**
      * This method is to initialize listeners
      */
-    private void initListeners() {
-        btn_login.setOnClickListener(this);
-        btn_register_p.setOnClickListener(this);
-    }
 
     /**
      * This method is to initialize objects to be used
      */
     private void initObjects() {
-        databaseHelper = new DatabaseHelper(activity);
         inputValidation = new InputValidation(activity);
 
     }
@@ -77,49 +110,49 @@ public class LoginMitra extends AppCompatActivity implements View.OnClickListene
      *
      * @param v
      */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_login_mitra:
-                verifyFromSQLite();
-                break;
-            case R.id.btn_register_p:
-                // Navigate to RegisterActivity
-                Intent intentRegister = new Intent(getApplicationContext(), RegisterMitra1.class);
-                startActivity(intentRegister);
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.btn_login_mitra:
+//                verifyFromSQLite();
+//                break;
+//            case R.id.btn_register_p:
+//                // Navigate to RegisterActivity
+//                Intent intentRegister = new Intent(getApplicationContext(), RegisterMitra1.class);
+//                startActivity(intentRegister);
+//                break;
+//        }
+//    }
 
     /**
      * This method is to validate the input text fields and verify login credentials from SQLite
      */
-    private void verifyFromSQLite() {
-        if (!inputValidation.isInputEditTextFilled(et_email, textInputLayoutEmail, getString(R.string.error_message_email))) {
-            return;
-        }
-        if (!inputValidation.isInputEditTextEmail(et_email, textInputLayoutEmail, getString(R.string.error_message_email))) {
-            return;
-        }
-        if (!inputValidation.isInputEditTextFilled(et_email, textInputLayoutPassword, getString(R.string.error_message_email))) {
-            return;
-        }
-
-        if (databaseHelper.checkUser(et_email.getText().toString().trim()
-                , et_password.getText().toString().trim())) {
-
-
-            Intent accountsIntent = new Intent(activity, UsersListActivity.class);
-            accountsIntent.putExtra("EMAIL", et_email.getText().toString().trim());
-            emptyInputEditText();
-            startActivity(accountsIntent);
-
-
-        } else {
-            // Snack Bar to show success message that record is wrong
-            Snackbar.make(nestedScrollView, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show();
-        }
-    }
+//    private void verifyFromSQLite() {
+//        if (!inputValidation.isInputEditTextFilled(et_email, textInputLayoutEmail, getString(R.string.error_message_email))) {
+//            return;
+//        }
+//        if (!inputValidation.isInputEditTextEmail(et_email, textInputLayoutEmail, getString(R.string.error_message_email))) {
+//            return;
+//        }
+//        if (!inputValidation.isInputEditTextFilled(et_email, textInputLayoutPassword, getString(R.string.error_message_email))) {
+//            return;
+//        }
+//
+//        if (databaseHelper.checkUser(et_email.getText().toString().trim()
+//                , et_password.getText().toString().trim())) {
+//
+//
+//            Intent accountsIntent = new Intent(activity, UsersListActivity.class);
+//            accountsIntent.putExtra("EMAIL", et_email.getText().toString().trim());
+//            emptyInputEditText();
+//            startActivity(accountsIntent);
+//
+//
+//        } else {
+//            // Snack Bar to show success message that record is wrong
+//            Snackbar.make(nestedScrollView, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show();
+//        }
+//    }
 
     /**
      * This method is to empty all input edit text
--- app/src/main/java/gir/cuciindong/RegisterMitra1.java
+++ app/src/main/java/gir/cuciindong/RegisterMitra1.java
@@ -1,49 +1,99 @@
 package gir.cuciindong;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class RegisterMitra1 extends AppCompatActivity {
 
-    DatabaseHelper helper = new DatabaseHelper(this);
+    private Button login_p;
+    private Button register_p;
+    private EditText et_email;
+    private EditText et_password;
+    private EditText et_namaLaundry;
+    private EditText et_alamatLaundry;
+    private EditText et_teleponLaundry;
+
+    private String nama_laundry;
+    private String alamat_laundry;
+    private String no_tlp_laundry;
+    private String email;
+    private String password;
+
+    private void initializeLayout(){
+        login_p =  (Button) findViewById(R.id.btn_login_p);
+        register_p = (Button) findViewById(R.id.btn_register_mitra);
+        et_email = (EditText) findViewById(R.id.et_email_register_mitra);
+        et_password = (EditText) findViewById(R.id.et_password_register_mitra);
+        et_namaLaundry = (EditText) findViewById(R.id.et_nama_register_mitra);
+        et_alamatLaundry = (EditText) findViewById(R.id.et_alamat_register_mitra);
+        et_teleponLaundry = (EditText) findViewById(R.id.et_telepon_register_mitra);
+    }
+
+    private void registerUser(){
+        nama_laundry = et_namaLaundry.getText().toString();
+        alamat_laundry = et_alamatLaundry.getText().toString();
+        no_tlp_laundry = et_teleponLaundry.getText().toString();
+        email = et_email.getText().toString();
+        password = et_password.getText().toString();
+
+        if(TextUtils.isEmpty(nama_laundry)){
+            Toast.makeText(this, "Harap masukan nama laundry", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(alamat_laundry)){
+            Toast.makeText(this, "Harap masukan alamat laundry", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(no_tlp_laundry)){
+            Toast.makeText(this, "Harap masukan nomor telepon laundry", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Harap masukan email", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Harap masukan password", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_mitra1);
+
+        initializeLayout();
+
+
+        register_p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+                if (!email.isEmpty() && !password.isEmpty() && !nama_laundry.isEmpty() && !alamat_laundry.isEmpty() &&
+                        !no_tlp_laundry.isEmpty()){
+                    String insert_query = "INSERT INTO akun(email, password) VALUES(" +
+                            "'" + email + "', " +
+                            "'" + password + "')";
+                    DatabaseOpenHelper.execute(insert_query);
+                    Intent intent = new Intent(RegisterMitra1.this, LoginMitra.class);
+                    intent.putExtra("email", email);
+                    intent.putExtra("password", password);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        login_p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterMitra1.this, LoginMitra.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void onSignUpClick (View v){
-        if (v.getId() == R.id.btn_register_mitra){
-            EditText et_email = (EditText) findViewById(R.id.et_email_register_mitra);
-            EditText et_password = (EditText) findViewById(R.id.et_password_register_mitra);
-            EditText et_namaLaundry = (EditText) findViewById(R.id.et_nama_register_mitra);
-            EditText et_alamatLaundry = (EditText) findViewById(R.id.et_alamat_register_mitra);
-            EditText et_teleponLaundry = (EditText) findViewById(R.id.et_telepon_register_mitra);
-
-            String nama_laundry = et_namaLaundry.getText().toString();
-            String alamat_laundry = et_alamatLaundry.getText().toString();
-            String no_tlp_laundry = et_teleponLaundry.getText().toString();
-            String email = et_email.getText().toString();
-            String password = et_password.getText().toString();
-
-            Mitra c = new Mitra();
-            c.setEmail(email);
-            c.setPassword(password);
-            c.setNama_laundry(nama_laundry);
-            c.setAlamat_laundry(alamat_laundry);
-            c.setTlp_laundry(no_tlp_laundry);
-			
-			//jago lu bre
-            helper.insertContact(c);
-
-
-
-
-
-
-        }
-    }
-
 }
